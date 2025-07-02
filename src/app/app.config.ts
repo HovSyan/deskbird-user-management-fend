f9281f810245f63542e7356d98540818666813f1
Hovhannes Sahakyan <hovhannes@superannotate.com>
2025-07-03 00:04:01 +0200

    feat: add basic login form

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    importProvidersFrom(BrowserAnimationsModule),
+    provideRouter(routes),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        options: {
+            // darkModeSelector: 'no',
+        },
+        preset: Aura,
+      },
+    }),
+  ],
 };
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    FormsModule,
+    PanelModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    FloatLabelModule,
+    ButtonModule,
+    PasswordModule,
+    MessageModule
+  ],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
+    loginForm = new FormGroup({
+        email: new FormControl('', { validators: [Validators.required], nonNullable: true }),
+        password: new FormControl('')
+    })
 
+    isInvalid(controlName: keyof typeof this.loginForm.controls) {
+        const control = this.loginForm.get(controlName)!;
+        return control.invalid && control.dirty;
+    }
+
+    onSubmit() {
+        this.loginForm.updateValueAndValidity();
+    }
 }
