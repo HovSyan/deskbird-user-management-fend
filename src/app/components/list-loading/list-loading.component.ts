e1f8609050b6dfaa4b8e57ed1937274033f2c6af
Hovhannes Sahakyan <hovhannes@superannotate.com>
2025-07-04 16:39:34 +0200

    feat: add users list

--- src/app/components/list-loading/list-loading.component.ts
+++ src/app/components/list-loading/list-loading.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
-  selector: 'app-list-loading',
-  imports: [],
-  templateUrl: './list-loading.component.html',
-  styleUrl: './list-loading.component.scss'
+    selector: 'app-list-loading',
+    imports: [ProgressSpinnerModule],
+    templateUrl: './list-loading.component.html',
+    styleUrl: './list-loading.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListLoadingComponent {
-
-}
+export class ListLoadingComponent {}
--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { Store } from '@ngrx/store';
+import { usersListSelector } from '../../state/users-list/users-list.selectors';
+import { map } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-users-list',
-  imports: [],
-  templateUrl: './users-list.component.html',
-  styleUrl: './users-list.component.scss'
+    selector: 'app-users-list',
+    imports: [TableModule, CommonModule],
+    templateUrl: './users-list.component.html',
+    styleUrl: './users-list.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UsersListComponent {
+    private _store = inject(Store);
 
+    user$ = this._store.select(usersListSelector).pipe(map(({ list }) => list!));
 }
