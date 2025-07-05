c8a0f98c9b8297e46e456c3563d5e5c4aac16353
Hovhannes Sahakyan <hovhannes@superannotate.com>
2025-07-05 14:42:39 +0200

    feat: add search

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -8,15 +8,18 @@ import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { Store } from '@ngrx/store';
 import { usersListSelector } from '../../state/users-list/users-list.selectors';
-import { map } from 'rxjs';
+import { BehaviorSubject, combineLatest, map } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { authUserSelector } from '../../state/auth/auth.selectors';
-import { DELETE_USER_CONFIRM_MSG, UserRoles } from '../../constants';
+import { UserRoles } from '../../constants';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 import { UserDialogComponent } from '../user-dialog/user-dialog.component';
 import { User } from '../../services/user';
 import { MenuModule } from 'primeng/menu';
 import { MenuItem } from 'primeng/api';
 import { UserDeleteDialogComponent } from '../user-delete-dialog/user-delete-dialog.component';
+import { InputText } from 'primeng/inputtext';
 
 @Component({
     selector: 'app-users-list',
@@ -26,7 +29,10 @@ import { UserDeleteDialogComponent } from '../user-delete-dialog/user-delete-dia
         ButtonModule,
         UserDialogComponent,
         MenuModule,
-        UserDeleteDialogComponent
+        UserDeleteDialogComponent,
+        IconFieldModule,
+        InputIconModule,
+        InputText,
     ],
     templateUrl: './users-list.component.html',
     styleUrl: './users-list.component.scss',
@@ -35,9 +41,11 @@ import { UserDeleteDialogComponent } from '../user-delete-dialog/user-delete-dia
 export class UsersListComponent {
     private _store = inject(Store);
 
-    user$ = this._store
-        .select(usersListSelector)
-        .pipe(map(({ list }) => list!));
+    searchTerm$ = new BehaviorSubject('');
+    users$ = combineLatest([
+        this._store.select(usersListSelector),
+        this.searchTerm$.pipe(map((t) => t.toLowerCase())),
+    ]).pipe(map(([{ list }, searchTerm]) => list!.filter((u) => filterUser(u, searchTerm))));
     isAdmin$ = this._store
         .select(authUserSelector)
         .pipe(map((u) => u?.role.id === UserRoles.ADMIN));
@@ -48,12 +56,16 @@ export class UsersListComponent {
         {
             label: 'Edit',
             icon: 'pi pi-user-edit',
-            onClick: (user: User) => this.editDialog = { user }
+            onClick: (user: User) => (this.editDialog = { user }),
         },
         {
             label: 'Delete',
             icon: 'pi pi-times-circle',
-            onClick: (user: User) => this.deleteDialog = user
+            onClick: (user: User) => (this.deleteDialog = user),
         },
     ];
+}
+
+function filterUser(u: User, term: string) {
+    return Object.values((u)).some((v) => typeof v === 'string' && v.toLowerCase().includes(term));
 }
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 export const LOGOUT_FAILED_ERROR_MSG = 'We are sorry, but it seems we failed to log you out! If it is essential to log out try deleting browser cookies by cleaning browser history'
 export const UNKNOWN_ERROR_MSG = 'We are so sorry! But an unknown error occurred :(';
-export const PASSWORD_TOOLTIP = `Ideally this wouldn't be here. It seems weird nowadays setting up a password for somebody else especially if we don't have password change functionality 😅. \n\nSo this is here *ONLY* for demonstration and app easy testing.`
+export const PASSWORD_TOOLTIP = `Ideally this wouldn't be here. It seems weird nowadays setting up a password for somebody else especially if we don't have password change functionality 😅. \n\nSo this is here to test the app easily.`
 export const DELETE_USER_CONFIRM_MSG = 'Are you sure that you want to delete this user? This can\'t be reverted!';
 
 export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
